8d1fc6f2b2af9e64e64873316b39e0b723d7283b
Rodrigo Sandoval <sandovalrgg@gmail.com>
2026-03-13 22:31:10 -0600

    refactor(task-card): Mejora de estilos y estructura en tarjeta de tarea
    - Refactoriza el HTML y SCSS de task-card para una estructura más clara y moderna
    - Implementa enums y pipes para gestionar prioridades y visualización de fechas
    - Ajusta estilos de columna para mayor consistencia visual

--- src/app/shared/components/task-card/task-card.component.ts
+++ src/app/shared/components/task-card/task-card.component.ts
@@ -1,12 +1,29 @@
+import { DatePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { FreyTooltipDirective } from 'freya/tooltip';
+import { CalendarDays, LucideAngularModule } from 'lucide-angular';
 import { TaskModel } from 'src/app/core/services';
+import { PRIORITYLIST } from 'src/app/utils/constants';
+import { LookupPipe } from '../../pipes';
+
+enum PriorityEnum {
+  LOW = 1,
+  MEDIUM = 2,
+  HIGH = 3,
+}
 
 @Component({
   selector: 'app-task-card',
-  imports: [],
+  imports: [DatePipe, LucideAngularModule, FreyTooltipDirective, LookupPipe],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss',
 })
 export class TaskCardComponent {
   task = input<TaskModel>();
+  priorityList = [...PRIORITYLIST];
+  priorityEnum = PriorityEnum;
+
+  readonly icons = {
+    calendarDays: CalendarDays,
+  };
 }
